5b1dd5a191f4a7cea4565f1ad10b74b74573d046
Jona Al Farros <124805962+MetamorphosisDev@users.noreply.github.com>
2026-09-12 23:46:10 +0700

    feat: api service from backend

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,22 +1,46 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+
 import '../config/api_config.dart';
+import 'token_storage.dart';
 
 class ApiService {
-  static Future<List<dynamic>> getPosts(String token) async {
+  // GET JWT
+  static Future<String> getToken() async {
+    final token = await TokenStorage.getToken();
+
+    if (token == null) {
+      throw Exception('Token tidak ditemukan');
+    }
+    return token;
+  }
+
+  // Request GET
+  static Future<dynamic> get(String endpoint) async {
+    final token = await getToken();
     final response = await http.get(
-      Uri.parse('${ApiConfig.baseUrl}/posts'),
+      Uri.parse('${ApiConfig.baseUrl}$endpoint'),
       headers: {'Authorization': 'Bearer $token'},
     );
-
-    print('STATUS: ${response.statusCode}');
-    print('BODY: ${response.body}');
-
+    final data = jsonDecode(response.body);
     if (response.statusCode == 200) {
-      final json = jsonDecode(response.body);
-      return json['data'] ?? [];
+      return data['data'];
     }
+    throw Exception(data['message'] ?? 'Gagal mengambil data');
+  }
 
-    throw Exception('Gagal mengambil posts');
+  // GET ALL POSTS
+  static Future<List<dynamic>> getPosts() async {
+    return await get('/posts');
+  }
+
+  // GET POST BY ID
+  static Future<Map<String, dynamic>> getPostById(int id) async {
+    return await get('/posts/$id');
+  }
+
+  // GET ALL BOOKMARKS
+  static Future<List<dynamic>> getBookmarks() async {
+    return await get('/bookmarks');
   }
 }
